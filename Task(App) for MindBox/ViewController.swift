f0516b3f8257a0735454c0af76591388b82b1145
ivan530i <pchelnikov79@gmail.com>
2024-02-08 19:27:46 +0300

    Initial Commit

---
+++ Task(App) for MindBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task(App) for MindBox
+//
+//  Created by Иван on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
